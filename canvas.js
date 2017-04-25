fd16811695e68f252dd5dbc5e013459dd9675a9b
Wes <paperziggurat@gmail.com>
2017-04-25 18:11:17 -0500

    animation

--- canvas.js
+++ canvas.js
@@ -5,27 +5,27 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+/*c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
 c.fillStyle = 'rgba(0, 0, 255, 0.5)';
 c.fillRect(100, 500, 50, 50);
 c.fillStyle = 'rgba(0, 255, 0, 0.5)';
 c.fillRect(200, 700, 40, 20);
 console.log(canvas);
-console.log(document.defaultView);
+console.log(document.defaultView);*/
 
 //Line
-c.beginPath();
+/*c.beginPath();
 c.moveTo(50, 300);
 c.lineTo(300, 100);
 c.lineTo(400, 300);
 c.lineTo(500, 100);
 c.strokeStyle = "#fa34a3";
-c.stroke();
+c.stroke();*/
 
 
 //Arc / circle
-for (var i = 0; i < 3; i++) {
+/*for (var i = 0; i < 3; i++) {
   var x = Math.random() * window.innerWidth;
   var y = Math.random() * window.innerHeight;
   c.beginPath();
@@ -33,3 +33,76 @@ for (var i = 0; i < 3; i++) {
   c.strokeStyle = 'blue';
   c.stroke();
 }
+*/
+
+
+
+function Circle(x, y, r, dx, dy) 
+{
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.dx = dx;
+	this.dy = dy;
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+		c.strokeStyle = 'blue';
+		c.fill();
+		c.stroke();
+	}
+
+	this.update = function() {
+		if (this.x + this.r > innerWidth || (this.x - this.r) < 0) {
+			this.dx = -this.dx;
+		} 
+
+		if (this.y + this.r > innerHeight || (this.y - this.r) < 0) {
+			this.dy = -this.dy
+		}
+
+		this.x += this.dx;
+		this.y += this.dy;	
+
+		this.draw();		
+	}
+}
+
+
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+	var r = 30;
+	var x = Math.random() * (innerWidth - r * 2) + r;
+	var y = Math.random() * (innerHeight - r * 2) + r;
+	var dx = (Math.random() - 0.5) * 2;
+	var dy = (Math.random() - 0.5) * 2;
+
+	circleArray.push(new Circle(x, y, r, dx, dy));
+}
+
+console.log(circleArray);
+
+
+function animate() 
+{
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, innerWidth, innerHeight);
+
+	for (var i = 0; i < circleArray.length; i++) {
+		circleArray[i].update();
+	}
+}
+
+animate();
+
+
+
+
+
+
+
+
+
